6356a944557c6e36d8997e6f90b6450479914d39
Joshua Lumanog <lumanog.joshua05@gmail.com>
2024-03-09 18:01:35 +0800

    intro to authorization done.

--- controllers/note.php
+++ controllers/note.php
@@ -4,8 +4,22 @@ $config = require('config.php');
 $db = new Database($config["database"]);
 
 $heading = "Note";
+$currentUserId = 4;
 
 
-$note = $db->query("select * from notes where id = ?", [$_GET['id']])->fetch();
+$note = $db->query("select * from notes where id = :id", [
+    'id' => $_GET['id']
+    ])->fetch();
+
+//check if note doesn't exist
+//If note doesn't exist from table, then $note query holds a boolean value of FALSE
+if(!$note){
+    abort();
+}
+
+//check if note is being accessed by a different user
+if($note['user_id'] != $currentUserId){
+    abort(Response::FORBIDDEN);
+}
 
 require "views/note.view.php";
--- index.php
+++ index.php
@@ -2,24 +2,24 @@
 
 require 'functions.php';
 require 'Database.php';
+require 'Response.php';
 require 'router.php';
 
 
 
 
 
-
-
-/*
------------------------------------------------------
+/* ----------------------------------------------------- */
+/* $config = require('config.php');
+$db = new Database($config['database']);
 
 $id = $_GET['id']; // localhost:10/?id=2 - get query string value
-$query = "SELECT * FROM posts where id = ?"; // Use '?' to bind value. Never inline/concatenate user values/variables to SQL query string (SQL injection vulnerability)
+$query = "SELECT * FROM notes where user_id = ?"; // Use '?' to bind value. Never inline/concatenate user values/variables to SQL query string (SQL injection vulnerability)
 $posts = $db->query($query, [$id])->fetch(); // know the difference between fetch() and fetchALl() output
 
-dd($posts);
+dd($posts); */
 
-*/
+
 
 
 /* echo "<ul>";
